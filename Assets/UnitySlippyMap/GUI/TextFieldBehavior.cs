ad97fcab5bc01e9a2c02252cd47346ce3493ced6
Sang Kyu Jang <jsk6387@naver.com>
2016-08-12 12:19:18 +0900

    드론리스트 클릭시 드론 정보 변경

---
+++ Assets/UnitySlippyMap/GUI/TextFieldBehavior.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using GcsProject.Controller;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+public class TextFieldBehavior : MonoBehaviour,IPointerDownHandler {
+    private int selected;
+    private GcsController controller;
+    private Text droneKey;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	void Awake()
+    {
+        controller = GameObject.Find("GameObject").GetComponent<GcsController>();
+        droneKey = GameObject.Find("Key").GetComponent<Text>();
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    /// <summary>
+    /// 마우스 클릭 이벤트
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        selected = (int)-(gameObject.transform.localPosition.y + 15) / 15;
+        print(selected);
+        controller.GetDroneInfo(selected - 1);
+        droneKey.text = (selected - 1).ToString();
+    }
+}
